ad0dbc30a7a4af97f32c5e8581a7dc33bb32fdea
Daniel Houle <daniel.m.houle@gmail.com>
2020-03-01 20:16:09 -0600

    rename

--- reactive-api/src/main/java/com/fnbo/reactiveapi/client/DataClient.java
+++ reactive-api/src/main/java/com/fnbo/reactiveapi/client/DataClient.java
@@ -2,12 +2,8 @@ package com.fnbo.reactiveapi.client;
 
 import com.fnbo.reactiveapi.model.Comment;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -15,14 +11,14 @@ import static org.springframework.http.HttpMethod.GET;
 
 @Service
 public class DataClient {
-    private final RestTemplate webClient;
+    private final RestTemplate restTemplate;
 
-    public DataClient(RestTemplate webClient) {
-        this.webClient = webClient;
+    public DataClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public List<Comment> getAllComments() {
-        return webClient.exchange("http://localhost:8565/api/v1/comments", GET, null,
+        return restTemplate.exchange("http://localhost:8565/api/v1/comments", GET, null,
                 new ParameterizedTypeReference<List<Comment>>() {}).getBody();
     }
 }
